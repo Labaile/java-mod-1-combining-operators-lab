ca7fe2971faa4d83a268582eb79c2bc061e1e4f6
Labaile <Labaile1@gmail.com>
2022-06-23 15:28:00 -0400

    Add files via upload
    // To the user: to display the AND logic, change both variables to "true"
    //                     to display the OR logic, change one of the variable to "false"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
-    }
+
+       // To the user: to display the AND logic, change both variables to "true"
+       //              to display the OR logic, change one of the variable to "false"
+        
+       boolean r, s;
+
+        r = true;
+        s = true;
+
+        //and logic
+        if (r == true && s == true ){
+            System.out.println(" Truth Table for && Operator ");
+            System.out.println(" true && true = true");
+            System.out.println(" true && false = false");
+            System.out.println(" false && false = false");
+            System.out.println(" false && true = false ");
+        } 
+        // or logic
+            else if(r == true || s == true) {
+            System.out.println(" Truth Table for || Operator ");
+            System.out.println(" true || true = true");
+            System.out.println(" true || false = true");
+            System.out.println(" false || false = false");
+            System.out.println(" false || true = true ");
+               
+            }else{
+                return;
+            }
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        }
 }
+        
